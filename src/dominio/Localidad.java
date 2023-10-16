e8d63ea84716aee0233444e41dcdac117a8eff0f
Javier Fernández del Vallado <javier.fernandezvalladobaldo@usp.ceu.es>
2023-10-16 11:37:17 +0200

    codigo antes de compilar

--- src/dominio/Localidad.java
+++ src/dominio/Localidad.java
@@ -1,6 +1,40 @@
 package dominio;
-puclic class Localidad{
-private string nombre;
-public Localidad(string nombre);
+
+public class Localidad{
+        private String nombre;
+        private String numerodeHabitantes;
+        private int habitantes;
+
+        public Localidad(String nombre, String numerodeHabitantes){
+                this.nombre = nombre;
+                this.numerodeHabitantes = numerodeHabitantes;
+                this.habitantes = habitantes;
+
+        }
+        public void setNombre(String nombre){
+                this.nombre = nombre;
+        }
+        public void setNumerodeHabitantes(String numerodeHabitantes){
+                this.numerodeHabitantes = numerodeHabitantes;
+        }
+
+        public String getNombre(){
+                return nombre;
+        }
+        public String getNumerodeHabitantes(){
+                return numerodeHabitantes;
+        }
+
+        public int getHabitantes(){
+                return habitantes;
+
+        }
+
+   public String toString() {
+        return "Localidad: " + nombre + "Numero de habitantes: " + numerodeHabitantes + "Habitantes: " + habitantes;
 
 
+
+}
+
+}
